5ef4f38c8e8fa140350799d939fc56b46ed53d01
venkateshreddy <kvredy87@gmail.com>
2018-12-22 13:17:11 +0530

    Added two new URLs for subjects and marks

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
 import students from './students'
+import subjects from './subjects'
+import marks from './marks'
 
 const router = new Router()
 
 router.use('/students', students)
+router.use('/subjects', subjects)
+router.use('/marks', marks)
 
 export default router
